ee8a6b8393f562fe1f3c0040c2f9cb6293355cd0
jcplist <91445595+jcplist@users.noreply.github.com>
2023-06-13 20:01:50 +0800

    Eval plot

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-import pandas as pd 
-import numpy as np
 import matplotlib.pyplot as plt
-import sys 
+import pandas as pd
 
-filename = sys.argv[1]
-prediction_name = filename.split('/')[2][:-4]
-
-df = pd.read_csv(filename)
-ids = df['id'].values 
-y = df['Danceability'].values
-
-bins = [i-0.5 for i in range(11)]
-plt.xticks(range(10))
-plt.hist(y, bins=bins, align='mid')
-plt.xlabel('Danceability')
-plt.ylabel('Count')
-plt.title(f'{prediction_name} Histogram')
-
-plt.savefig(f'{filename[:-4]}-hist.png', dpi=300)
+Eval = pd.read_csv("score.csv").iloc[:,0]
+Public = pd.read_csv("score.csv").iloc[:,1]
+plt.scatter(Eval, Public)
+plt.savefig("Eval.png", dpi=300)
